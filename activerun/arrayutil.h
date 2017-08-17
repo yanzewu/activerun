d5f943ce433b0754af947e6264ac2927d508de88
Yanze Wu <flipboards@github.com>
2017-08-16 23:30:32 -0700

    seems avx doesn't work very well

--- activerun/arrayutil.h
+++ activerun/arrayutil.h
@@ -5,11 +5,11 @@
 
 #include "vec.h"
 #include <vector>
-#include <immintrin.h>
+//#include <immintrin.h>
 
 inline void array_add_double(const double* src, double* dst, unsigned int size) {
 	
-	unsigned int i, j;
+/*	unsigned int i, j;
 	for (i = 0; i < size / 32; i++) {
 		_mm256_store_pd(dst + 32 * i + 0, _mm256_addsub_pd(_mm256_load_pd(src + 32 * i + 0), _mm256_load_pd(dst + 32 * i + 0)));
 		_mm256_store_pd(dst + 32 * i + 4, _mm256_addsub_pd(_mm256_load_pd(src + 32 * i + 4), _mm256_load_pd(dst + 32 * i + 4)));
@@ -23,13 +23,13 @@ inline void array_add_double(const double* src, double* dst, unsigned int size) 
 
 	for (j = i * 32; j < size - 4; j += 4) {
 		_mm256_store_pd(dst + j, _mm256_addsub_pd(_mm256_load_pd(src + j), _mm256_load_pd(dst + j)));
-	}
+	}*/
 
-	for (; j < size; j++) {
+	for (int j = 0; j < size; j++) {
 		dst[j] += src[j];
 	}
 }
-
+/*
 inline void array_mul_double(const double* src1, const double* src2, double* dst, unsigned int size) {
 
 	unsigned int i, j;
@@ -51,17 +51,17 @@ inline void array_mul_double(const double* src1, const double* src2, double* dst
 	for (; j < size; j++) {
 		dst[j] += src1[j];
 	}
-}
+}*/
 
 
 // speed like a wind
 inline void array_add(const std::vector<Vec2>& src, std::vector<Vec2>& dst) {
 	array_add_double((double*)&src[0], (double*)&dst[0], 2 * src.size());
 }
-
+/*
 inline void array_mul(const std::vector<Vec2>& src1, const std::vector<Vec2>& src2, std::vector<Vec2>& dst) {
 	array_mul_double((double*)&src1[0], (double*)&src2[0], (double*)&dst[0], 2 * src1.size());
-}
+}*/
 
 #endif // !ARRAYUTIL_H
 
